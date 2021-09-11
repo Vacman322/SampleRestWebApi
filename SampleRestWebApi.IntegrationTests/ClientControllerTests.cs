38187f174c6014f8e310624949f4e25cf3f9819a
Nikita Lazarev <Nikitta92@yandex.ru>
2021-09-11 20:33:42 +0300

    Added Integration testing

---
+++ SampleRestWebApi.IntegrationTests/ClientControllerTests.cs
@@ -0,0 +1,54 @@
+﻿using SampleRestWebApi.Contracts.V1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using System.Net;
+using System.Net.Http;
+using SampleRestWebApi.Domain;
+using NUnit.Framework;
+using SampleRestWebApi.Contracts.V1.Requests;
+
+namespace SampleRestWebApi.IntegrationTests
+{
+    class ClientControllerTests : IntegrationTest
+    {
+        [TearDown]
+        public void Cleanup()
+        {
+            Dispose();
+        }
+
+        [Test]
+        public async Task GetAll_WithoutAnyClient_ReturnsEmptyResponse()
+        {
+            // Arrange
+            await AuthenticateAsync();
+
+            // Act
+            var response = await TestClient.GetAsync(ApiRoutes.Clients.GetAll);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            (await response.Content.ReadAsAsync<List<Client>>()).Should().BeEmpty();
+        }
+
+        [Test]
+        public async Task Get_ReturnsClient_WhenClientExistsInDatabase()
+        {
+            // Arrange
+            await AuthenticateAsync();
+            var createdClient = await CreateClientAsync(new CreateClientRequest { Name = "Vasya" });
+
+            // Act
+            var response = await TestClient.GetAsync(ApiRoutes.Clients.Get.Replace("{clientId}", createdClient.Id.ToString()));
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var returnedClient = await response.Content.ReadAsAsync<Client>();
+            returnedClient.Id.Should().Be(createdClient.Id);
+            returnedClient.Name.Should().Be("Vasya");
+        }
+    }
+}
---
+++ SampleRestWebApi.IntegrationTests/IntegrationTest.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using SampleRestWebApi.Contracts.V1;
+using SampleRestWebApi.Contracts.V1.Requests;
+using SampleRestWebApi.Contracts.V1.Responses;
+using SampleRestWebApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleRestWebApi.IntegrationTests
+{
+    class IntegrationTest : IDisposable
+    {
+        protected readonly HttpClient TestClient;
+        private readonly IServiceProvider _serviceProvider;
+
+        protected IntegrationTest()
+        {
+            var appFactory = new CustomWebApplicationFactory<Startup>();
+            _serviceProvider = appFactory.Services;
+            TestClient = appFactory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            using var serviceScope = _serviceProvider.CreateScope();
+            var context = serviceScope.ServiceProvider.GetService<DataContext>();
+            context.Database.EnsureDeleted();
+        }
+
+        protected async Task AuthenticateAsync()
+        {
+            TestClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("bearer", await GetJWTAsync());
+        }
+
+        protected async Task<ClientResponse> CreateClientAsync(CreateClientRequest request)
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Clients.Create, request);
+
+            return await response.Content.ReadAsAsync<ClientResponse>();
+        }
+
+        private async Task<string> GetJWTAsync()
+        {
+            var response = await TestClient.PostAsJsonAsync(ApiRoutes.Identity.Register, new UserRegistrationRequest
+            {
+                Email = "test@test.tessst",
+                Password = "PasswordPls123!"
+            });
+
+            var registartionResponse = await response.Content.ReadAsAsync<AuthSuccessResponse>();
+            return registartionResponse.Token;
+        }
+    }
+
+    public class CustomWebApplicationFactory<TStartup>
+    : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices(services =>
+            {
+                var descriptor = services.SingleOrDefault(
+                    d => d.ServiceType ==
+                        typeof(DbContextOptions<DataContext>));
+
+                services.Remove(descriptor);
+
+                services.AddDbContext<DataContext>(options =>
+                {
+                    options.UseInMemoryDatabase("InMemoryDbForTesting");
+                });
+
+                //var sp = services.BuildServiceProvider();
+
+                //using (var scope = sp.CreateScope())
+                //{
+                //    var scopedServices = scope.ServiceProvider;
+                //    var db = scopedServices.GetRequiredService<DataContext>();
+                //    var logger = scopedServices
+                //        .GetRequiredService<ILogger<CustomWebApplicationFactory<TStartup>>>();
+
+                //    //db.Database.EnsureCreated();
+
+                //}
+            });
+        }
+    }
+}
